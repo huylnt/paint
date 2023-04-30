33eb084094790d717dbead621a43264506f27fa8
20127037 <20127037@student.hcmus.edu.vn>
2023-04-30 09:08:48 +0700

    Done filling element
    Next: add text

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import { useState, useLayoutEffect, useContext } from 'react'
 import { Tabs, Tab, TabPanels, TabPanel, TabList, Flex, Box, Image, Button, Menu, MenuButton, MenuList, MenuItem, Tooltip, Divider } from '@chakra-ui/react'
 import { FaRegSave } from 'react-icons/fa'
 import { AiOutlineFormatPainter } from 'react-icons/ai'
-import { FiTrash } from 'react-icons/fi'
 import { BsPencil } from 'react-icons/bs'
+import { CiText } from 'react-icons/ci'
 
-import {originCanvasContext, CanvasContext} from 'context/CanvasContext'
+import { CanvasContext } from 'context/CanvasContext'
 import Canvas from 'Canvas'
 import Dialog from 'component/Dialog'
 
@@ -28,6 +28,7 @@ const App = () => {
   const [strokeType, setStrokeType] = useState('CONTINUOUS')
   const [dialogContext, setDialogContext] = useState()
   const [refresher, setRefresher] = useState(0)
+
   useLayoutEffect(() => {
     if (!object) {
       setObject('LINE')
@@ -39,7 +40,7 @@ const App = () => {
     <ChakraProvider theme={theme}>
       <CanvasContext>
         <Box>
-          <Flex justify='space-between' align='center' marginBottom='15px' position='fixed' width='100%' padding='20px 20px 30px 20px' boxShadow='2xl' borderRadius='20px'>
+          <Flex justify='space-between' align='center' marginBottom='15px' position='fixed' width='100%' padding='20px 20px 30px 20px' boxShadow='1px 12px 24px 9px rgba(57,128,146,0.27)' borderRadius='20px'>
             <Tabs variant='soft-rounded' colorScheme='blue' size='lg' defaultIndex={0} height='18vh'>
               <TabList paddingBottom='5px'>
                 <Tab>Objects</Tab>
@@ -48,46 +49,66 @@ const App = () => {
               <TabPanels>
                 <TabPanel>
                   <Flex gap='30px'>
-                    <Box as='Button' padding='10px' bg='white' borderRadius='full' width='fit-content' height='fit-content' boxShadow='dark-lg' borderWidth='3px' opacity={(object === 'LINE') ? '1' : '0.5'} borderColor={(object === 'LINE' ? 'accent' : 'white')} onClick={() => setObject('LINE')}>
+                    <Box as='button' padding='10px' bg='white' borderRadius='full' width='fit-content' height='fit-content' boxShadow='dark-lg' borderWidth='3px' opacity={(object === 'LINE') ? '1' : '0.5'} borderColor={(object === 'LINE' ? 'accent' : 'white')} onClick={() => setObject('LINE')}>
                       <Tooltip label='Line' bg='accent' borderRadius='full'>
                         <Image src='icon_line.png' boxSize='30px' />
                       </Tooltip>
                     </Box>
 
-                    <Box as='Button' padding='10px' bg='white' borderRadius='full' width='fit-content' height='fit-content' boxShadow='dark-lg' borderWidth='3px' opacity={(object === 'RECTANGLE') ? '1' : '0.5'} borderColor={(object === 'RECTANGLE' ? 'accent' : 'white')} onClick={() => setObject('RECTANGLE')}>
+                    <Box as='button' padding='10px' bg='white' borderRadius='full' width='fit-content' height='fit-content' boxShadow='dark-lg' borderWidth='3px' opacity={(object === 'RECTANGLE') ? '1' : '0.5'} borderColor={(object === 'RECTANGLE' ? 'accent' : 'white')} onClick={() => setObject('RECTANGLE')}>
                       <Tooltip label='Rectangle' bg='accent' borderRadius='full'>
                         <Image src='icon_rectangle.png' boxSize='30px' />
                       </Tooltip>
                     </Box>
 
-                    <Box as='Button' padding='10px' bg='white' borderRadius='full' width='fit-content' height='fit-content' boxShadow='dark-lg' borderWidth='3px' opacity={(object === 'ELLIPSE') ? '1' : '0.5'} borderColor={(object === 'ELLIPSE' ? 'accent' : 'white')}  onClick={() => setObject('ELLIPSE')}>
+                    <Box as='button' padding='10px' bg='white' borderRadius='full' width='fit-content' height='fit-content' boxShadow='dark-lg' borderWidth='3px' opacity={(object === 'ELLIPSE') ? '1' : '0.5'} borderColor={(object === 'ELLIPSE' ? 'accent' : 'white')} onClick={() => setObject('ELLIPSE')}>
                       <Tooltip label='Ellipse' bg='accent' borderRadius='full'>
                         <Image src='icon_ellipse.png' boxSize='30px' />
+                      </Tooltip>
+                    </Box>
+
+                    <Box as='button' padding='10px' bg='white' borderRadius='full' width='fit-content' height='fit-content' boxShadow='dark-lg' borderWidth='3px' opacity={(object === 'TEXT') ? '1' : '0.5'} borderColor={(object === 'TEXT' ? 'accent' : 'white')} onClick={() => setObject('TEXT')}>
+                      <Tooltip label='Text' bg='accent' borderRadius='full'>
+                        <div>
+                          <CiText size='30px' />
+                        </div>
+                      </Tooltip>
+                    </Box>
+
+                    <Divider orientation='vertical' width='5px' height='55px' bg='primary' />
+
+                    <Box as='button' padding='10px' bg='white' borderRadius='full' width='fit-content' height='fit-content' boxShadow='dark-lg' borderWidth='3px' opacity={(object === 'SELECT') ? '1' : '0.5'} borderColor={(object === 'SELECT' ? 'accent' : 'white')} onClick={() => setObject('SELECT')}>
+                      <Tooltip label='Select' bg='accent' borderRadius='full'>
+                        <Image src='icon_select.png' boxSize='30px' />
+                      </Tooltip>
+                    </Box>
+
+                    <Box as='button' padding='10px' bg='white' borderRadius='full' width='fit-content' height='fit-content' boxShadow='dark-lg' borderWidth='3px' opacity={(object === 'FILL') ? '1' : '0.5'} borderColor={(object === 'FILL' ? 'accent' : 'white')} onClick={() => setObject('FILL')}>
+                      <Tooltip label='Fill' bg='accent' borderRadius='full'>
+                        <div>
+                          <AiOutlineFormatPainter size='30px' />
+                        </div>
                       </Tooltip>
                     </Box>
 
                     <Divider orientation='vertical' width='5px' height='55px' bg='primary' />
 
                     <Box as='Button' padding='10px' bg='white' borderRadius='full' width='fit-content' height='fit-content' boxShadow='dark-lg' borderWidth='3px' onClick={() => {
-                          setObject('EMPTY')
-                          setRefresher(prev => prev + 1)
-                        }}>
+                      setObject('TRASH')
+                      setRefresher(prev => prev + 1)
+                    }}>
                       <Tooltip label='Clear all' bg='accent' borderRadius='full'>
                         <Image src='icon_clear.png' boxSize='30px' />
-                      </Tooltip>
-                    </Box>
-
-                    <Box as='Button' padding='10px' bg='white' borderRadius='full' width='fit-content' height='fit-content' boxShadow='dark-lg' borderWidth='3px' opacity={(object === 'SELECT') ? '1' : '0.5'} borderColor={(object === 'SELECT' ? 'accent' : 'white')} onClick={() => setObject('SELECT')}>
-                      <Tooltip label='Select' bg='accent' borderRadius='full'>
-                        <Image src='icon_select.png' boxSize='30px' />
                       </Tooltip>
                     </Box>
                   </Flex>
 
 
+
+
                 </TabPanel>
                 <TabPanel>
-                  <Flex gap='30px'>
+                  <Flex gap='30px' marginTop='10px'>
                     <Menu>
                       <MenuButton as={Button} variant='outline' colorScheme='blue' leftIcon={<BsPencil />}>Configure pen</MenuButton>
                       <MenuList boxShadow='2xl'>
@@ -95,7 +116,7 @@ const App = () => {
                         <MenuItem onClick={() => setDialogContext("PEN_STROKE_TYPE")}>Change stroke type</MenuItem>
                       </MenuList>
                     </Menu>
-                    <Button leftIcon={<AiOutlineFormatPainter />} variant='outline' colorScheme='blue'>Fill object</Button>
+
                     <Menu>
                       <MenuButton as={Button} variant='outline' colorScheme='blue' leftIcon={<FaRegSave />}>Save</MenuButton>
                       <MenuList boxShadow='2xl'>
@@ -111,7 +132,7 @@ const App = () => {
 
             <Flex flexFlow='column' gap='10px' align='center'>
               <Box fontFamily='Alkatra'>Selected color</Box>
-              <Box as='Button' bg={color} borderRadius='full' padding='20px 50px' onClick={() => setDialogContext("PEN_COLOR")}></Box>
+              <Box as='button' bg={color} borderRadius='full' padding='20px 50px' onClick={() => setDialogContext("PEN_COLOR")}></Box>
             </Flex>
 
           </Flex>
--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -3,6 +3,65 @@ import { RoughCanvas } from 'roughjs/bin/canvas'
 import { originCanvasContext } from 'context/CanvasContext'
 import { useContext } from 'react'
 
+const corConverter = (mouseX1, mouseY1, mouseX2, mouseY2) => {
+  return {
+    canvasX1: mouseX1,
+    canvasY1: mouseY1,
+    canvasX2: mouseX2,
+    canvasY2: mouseY2
+  }
+}
+
+const strokeTypeConverter = (strokeType) => {
+  if (strokeType === 'CONTINUOUS') return []
+  else if (strokeType === 'DASH_DASH') return [5, 15]
+  else if (strokeType === 'DASH_DOT_DASH') return [5, 15, 1, 15, 5, 15, 1, 15]
+}
+
+const ensureHighQuality = (canvas, canvasContext) => {
+  // Get the DPR and size of the canvas
+  const dpr = window.devicePixelRatio;
+  const rect = canvas.getBoundingClientRect();
+
+  // Set the "actual" size of the canvas
+  canvas.width = rect.width * dpr;
+  canvas.height = rect.height * dpr;
+
+  // Scale the context to ensure correct drawing operations
+  canvasContext.scale(dpr, dpr);
+
+  // Set the "drawn" size of the canvas
+  canvas.style.width = `${rect.width}px`;
+  canvas.style.height = `${rect.height}px`;
+}
+
+const distance = (a, b) => Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2))
+
+const isWithinLine = (figure, mouseX, mouseY) => {
+  const a = { x: figure.mouseX1, y: figure.mouseY1 }
+  const b = { x: figure.mouseX2, y: figure.mouseY2 }
+  const c = { x: mouseX, y: mouseY }
+  const offset = distance(a, b) - (distance(a, c) + distance(b, c))
+  return Math.abs(offset) < 1 ? true : false
+}
+
+const isWithinRectangle = (figure, mouseX, mouseY) => {
+  const minX = Math.min(figure.mouseX1, figure.mouseX2)
+  const maxX = Math.max(figure.mouseX1, figure.mouseX2)
+  const minY = Math.min(figure.mouseY1, figure.mouseY2)
+  const maxY = Math.max(figure.mouseY1, figure.mouseY2)
+  return mouseX >= minX && mouseX <= maxX && mouseY >= minY && mouseY <= maxY
+}
+
+const isWithinEllipse = (figure, mouseX, mouseY) => {
+  const origin = { x: figure.mouseX1, y: figure.mouseY1 }
+  const mouse = { x: mouseX, y: mouseY }
+  const boudaryWidth = { x: figure.mouseX2 - figure.mouseX1, y: figure.mouseY1 }
+  const boundaryHeight = { x: figure.mouseX1, y: figure.mouseY2 - figure.mouseY1 }
+
+  return distance(origin, mouse) < distance(origin, boudaryWidth) && distance(origin, mouse) < distance(origin, boundaryHeight)
+}
+
 const Canvas = ({ object, color, penWidth, penStrokeType, refresher, setRefresher }) => {
   const canvasContext = useContext(originCanvasContext)
   const { figures, setFigures } = canvasContext
@@ -10,73 +69,48 @@ const Canvas = ({ object, color, penWidth, penStrokeType, refresher, setRefreshe
   const [isDrawing, setIsDrawing] = useState(false)
   const [selectedElement, setSelectedElement] = useState()
   let roughCanvas
-  const [offsetMouse, setOffsetMouse] = useState({
+  const [offsetMouse, _] = useState({
     x: 0,
     y: 0
   })
 
-  const corConverter = (mouseX1, mouseY1, mouseX2, mouseY2) => {
-    return {
-      canvasX1: mouseX1,
-      canvasY1: mouseY1,
-      canvasX2: mouseX2,
-      canvasY2: mouseY2
-    }
-  }
-
-  const strokeTypeConverter = (strokeType) => {
-    if (strokeType === 'CONTINUOUS') return []
-    else if (strokeType === 'DASH_DASH') return [5, 15]
-    else if (strokeType === 'DASH_DOT_DASH') return [5, 15, 1, 15, 5, 15, 1, 15]
-  }
-
-  const ensureHighQuality = (canvas, canvasContext) => {
-    // Get the DPR and size of the canvas
-    const dpr = window.devicePixelRatio;
-    const rect = canvas.getBoundingClientRect();
-
-    // Set the "actual" size of the canvas
-    canvas.width = rect.width * dpr;
-    canvas.height = rect.height * dpr;
-
-    // Scale the context to ensure correct drawing operations
-    canvasContext.scale(dpr, dpr);
-
-    // Set the "drawn" size of the canvas
-    canvas.style.width = `${rect.width}px`;
-    canvas.style.height = `${rect.height}px`;
-  }
-
-  const distance = (a, b) => Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2))
-
-  const isWithinLine = (figure, mouseX, mouseY) => {
-    const a = { x: figure.mouseX1, y: figure.mouseY1 }
-    const b = { x: figure.mouseX2, y: figure.mouseY2 }
-    const c = { x: mouseX, y: mouseY }
-    const offset = distance(a, b) - (distance(a, c) + distance(b, c))
-    return Math.abs(offset) < 1 ? true : false
-  }
-
-  const isWithinRectangle = (figure, mouseX, mouseY) => {
-    const minX = Math.min(figure.mouseX1, figure.mouseX2)
-    const maxX = Math.max(figure.mouseX1, figure.mouseX2)
-    const minY = Math.min(figure.mouseY1, figure.mouseY2)
-    const maxY = Math.max(figure.mouseY1, figure.mouseY2)
-    return mouseX >= minX && mouseX <= maxX && mouseY >= minY && mouseY <= maxY
-  }
-
-  const isWithinEllipse = (figure, mouseX, mouseY) => {
-    const origin = { x: figure.mouseX1, y: figure.mouseY1 }
-    const mouse = { x: mouseX, y: mouseY }
-    const boudaryWidth = { x: figure.mouseX2 - figure.mouseX1, y: figure.mouseY1 }
-    const boundaryHeight = { x: figure.mouseX1, y: figure.mouseY2 - figure.mouseY1 }
-
-    return distance(origin, mouse) < distance(origin, boudaryWidth) && distance(origin, mouse) < distance(origin, boundaryHeight)
-  }
-
   const isWithinElement = (mouseX, mouseY) => {
     const selectedElement = figures.find(figure => isWithinLine(figure, mouseX, mouseY) || isWithinRectangle(figure, mouseX, mouseY) || isWithinEllipse(figure, mouseX, mouseY))
     return selectedElement
+  }
+
+  const transitElement = (clientX, clientY) => {
+    if (selectedElement.objectType === 'LINE') {
+      const distanceX = selectedElement.mouseX2 - selectedElement.mouseX1
+      const distanceY = selectedElement.mouseY2 - selectedElement.mouseY1
+      selectedElement.mouseX1 = clientX - offsetMouse.x
+      selectedElement.mouseY1 = clientY - offsetMouse.y
+      selectedElement.mouseX2 = clientX - offsetMouse.x + distanceX
+      selectedElement.mouseY2 = clientY - offsetMouse.y + distanceY
+    }
+
+    else if (selectedElement.objectType === 'RECTANGLE') {
+      const width = selectedElement.mouseX2  - selectedElement.mouseX1
+      const height = selectedElement.mouseY2 - selectedElement.mouseY1
+      selectedElement.mouseX1 = clientX - offsetMouse.x
+      selectedElement.mouseY1 = clientY - offsetMouse.y
+      selectedElement.mouseX2 = clientX - offsetMouse.x + width
+      selectedElement.mouseY2 = clientY - offsetMouse.y + height
+    }
+
+    else if (selectedElement.objectType === 'ELLIPSE') {
+      const width = (selectedElement.mouseX2  - selectedElement.mouseX1)
+      const height = (selectedElement.mouseY2 - selectedElement.mouseY1)
+      selectedElement.mouseX1 = clientX - offsetMouse.x
+      selectedElement.mouseY1 = clientY - offsetMouse.y
+      selectedElement.mouseX2 = clientX - offsetMouse.x + width
+      selectedElement.mouseY2 = clientY - offsetMouse.y + height
+    }
+  }
+
+  const fillElement = () => {
+    selectedElement.fillColor = color
+    // setRefresher(prev => prev + 1)
   }
 
   useLayoutEffect(() => {
@@ -89,43 +123,43 @@ const Canvas = ({ object, color, penWidth, penStrokeType, refresher, setRefreshe
 
     if (figures.length < 1) return
 
-    if (object === 'EMPTY') {
+    if (object === 'TRASH') {
       figures.length = 0
       return
     }
 
     roughCanvas = new RoughCanvas(canvas)
 
-    figures.forEach(({ objectType, strokeColor, penWidth, strokeLineStyle, mouseX1, mouseY1, mouseX2, mouseY2 }) => {
+    figures.forEach(({ objectType, strokeColor, penWidth, strokeLineStyle, fillColor, mouseX1, mouseY1, mouseX2, mouseY2 }) => {
       const { canvasX1, canvasY1, canvasX2, canvasY2 } = corConverter(mouseX1, mouseY1, mouseX2, mouseY2)
       if (objectType === 'LINE') {
-        roughCanvas.line(canvasX1, canvasY1, canvasX2, canvasY2, { stroke: strokeColor, strokeWidth: penWidth, strokeLineDash: strokeLineStyle })
+        roughCanvas.line(canvasX1, canvasY1, canvasX2, canvasY2, { stroke: (fillColor) ? fillColor : strokeColor, strokeWidth: penWidth, strokeLineDash: strokeLineStyle })
       }
       else if (objectType === 'RECTANGLE') {
         const width = canvasX2 - canvasX1
         const height = canvasY2 - canvasY1
-        roughCanvas.rectangle(canvasX1, canvasY1, width, height, { stroke: strokeColor, strokeWidth: penWidth, strokeLineDash: strokeLineStyle })
+        roughCanvas.rectangle(canvasX1, canvasY1, width, height, { stroke: strokeColor, strokeWidth: penWidth, strokeLineDash: strokeLineStyle, fill: fillColor, fillWeight: 5 })
       }
       else if (objectType === 'ELLIPSE') {
         const width = (canvasX2 - canvasX1) * 2
         const height = (canvasY2 - canvasY1) * 2
-        roughCanvas.ellipse(canvasX1, canvasY1, width, height, { stroke: strokeColor, strokeWidth: penWidth, strokeLineDash: strokeLineStyle })
+        roughCanvas.ellipse(canvasX1, canvasY1, width, height, { stroke: strokeColor, strokeWidth: penWidth, strokeLineDash: strokeLineStyle, fill: fillColor })
       }
     })
-
   }, [refresher])
 
   const handleMouseDown = (event) => {
     setIsDrawing(true)
     const { clientX, clientY } = event
 
-    if (object !== 'SELECT')
+    if (object === 'LINE' || object === 'RECTANGLE' || object === 'ELLIPSE')
       setFigures(prev => [...prev, {
         id: figures.length,
         objectType: object,
         strokeColor: color,
         penWidth: penWidth,
         strokeLineStyle: strokeTypeConverter(penStrokeType),
+        fillColor: undefined,
         mouseX1: clientX,
         mouseY1: clientY,
         mouseX2: undefined,
@@ -138,6 +172,11 @@ const Canvas = ({ object, color, penWidth, penStrokeType, refresher, setRefreshe
         setSelectedElement(tempSelectedElement)
         offsetMouse.x = clientX - tempSelectedElement.mouseX1
         offsetMouse.y = clientY - tempSelectedElement.mouseY1
+
+        if (object === 'FILL') {
+          tempSelectedElement.fillColor = color
+          setRefresher(prev => prev + 1)
+        }
       }
     }
 
@@ -147,49 +186,19 @@ const Canvas = ({ object, color, penWidth, penStrokeType, refresher, setRefreshe
     if (!isDrawing) return
     const { clientX, clientY } = event
 
-    if (object !== 'SELECT') {
-      figures[figures.length - 1].mouseX2 = clientX
-      figures[figures.length - 1].mouseY2 = clientY
+    if (object === 'SELECT') {
+      if (selectedElement) transitElement(clientX, clientY)
     }
 
-    else if (object === 'SELECT') {
-      if (selectedElement) {
-        if (selectedElement.objectType === 'LINE') {
-          const distanceX = selectedElement.mouseX2 - selectedElement.mouseX1
-          const distanceY = selectedElement.mouseY2 - selectedElement.mouseY1
-          selectedElement.mouseX1 = clientX - offsetMouse.x
-          selectedElement.mouseY1 = clientY - offsetMouse.y
-          selectedElement.mouseX2 = clientX - offsetMouse.x + distanceX
-          selectedElement.mouseY2 = clientY - offsetMouse.y + distanceY
-        }
-
-        else if (selectedElement.objectType === 'RECTANGLE') {
-          const width = selectedElement.mouseX2  - selectedElement.mouseX1
-          const height = selectedElement.mouseY2 - selectedElement.mouseY1
-          selectedElement.mouseX1 = clientX - offsetMouse.x
-          selectedElement.mouseY1 = clientY - offsetMouse.y
-          selectedElement.mouseX2 = clientX - offsetMouse.x + width
-          selectedElement.mouseY2 = clientY - offsetMouse.y + height
-        }
-
-        else if (selectedElement.objectType === 'ELLIPSE') {
-          const width = (selectedElement.mouseX2  - selectedElement.mouseX1)
-          const height = (selectedElement.mouseY2 - selectedElement.mouseY1)
-          selectedElement.mouseX1 = clientX - offsetMouse.x
-          selectedElement.mouseY1 = clientY - offsetMouse.y
-          selectedElement.mouseX2 = clientX - offsetMouse.x + width
-          selectedElement.mouseY2 = clientY - offsetMouse.y + height
-        }
-      }
+    else if (object === 'LINE' || object === 'RECTANGLE' || object === 'ELLIPSE') {
+      figures[figures.length - 1].mouseX2 = clientX
+      figures[figures.length - 1].mouseY2 = clientY
     }
 
     setRefresher(prev => prev + 1)
   }
 
   const handleMouseUp = (event) => {
-    if (!isDrawing) return
-
-    const { clientX, clientY } = event
     setIsDrawing(false)
   }
 
